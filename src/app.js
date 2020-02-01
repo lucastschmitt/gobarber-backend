a256a0003bc99826d05da36a5ac4a227a98afc37
Lucas Testoni Schmitt <lucastestonischmitt@MacBook-Pro-de-Lucas.local>
2020-02-01 20:48:50 -0300

    Primeiro Commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import Youch from 'youch';
+import 'express-async-errors';
+import routes from './routes';
+
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, req, res, next) => {
+      if (process.env.NODE_ENVIROMENT === 'development') {
+        const errors = await new Youch(error, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal Server Error.' });
+    });
+  }
+}
+
+export default new App().server;
